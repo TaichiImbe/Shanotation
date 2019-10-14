c6f3c3ee9f7adc6808539daf092fd9315c3521a0
furukami <soranguriarumika@gmail.com>
2019-10-14 16:02:15 +0900

    logの追加

--- dev/js/pdf.js
+++ dev/js/pdf.js
@@ -53,7 +53,7 @@ function pageRender(pageNum) {
             // console.log(textContent);
             textContent.items.forEach(text => {
                 var tx = pdfjsLib.Util.transform(viewport.transform, text.transform);
-                // console.log(text);
+                console.log(text);
                 console.log(tx);   
             });
             // pdfjsLib.renderTextLayer({
