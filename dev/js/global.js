6cb9e551f4aac444d29256d672242b345c3c2176
furukami <soranguriarumika@gmail.com>
2019-12-01 01:17:19 +0900

    不要なプログラムの削除

--- dev/js/global.js
+++
@@ -1,9 +0,0 @@
-let fabric = require('fabric').fabric;
-global.Canvas = new fabric.Canvas('draw-area',{
-    isDrawingMode: true,
-    selection: true,
-    stateful: true
-});
-
-pageNum = 1;
-global.pageNum = this.pageNum; 
