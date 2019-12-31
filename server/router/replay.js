0a12717a8da15032ec2a678b74d98060b8c34423
furukami <soranguriarumika@gmail.com>
2019-12-31 23:52:06 +0900

    変更途中コミット

--- server/router/replay.js
+++ server/router/replay.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 router.route('/replay')
     .get((req, res, next) => {
-        res.render('./replay', { userName: req.body.userName ,pdfname:''});
+        res.render('./replay', { userName: req.body.userName ,pdfname:req.body.pdfName});
     })
     .post((req, res, next) => {
         res.render('./replay', { userName: req.body.userName, pdfname: req.body.pdfName });
